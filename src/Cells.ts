2435039d4884b4e4b726861acdb51cd5a692b080
hexpunk <10983817+hexpunk@users.noreply.github.com>
2026-03-14 19:12:13 -0500

    Switch to inputs for Cells

--- src/Cells.ts
+++ src/Cells.ts
@@ -45,6 +45,22 @@ export class Cells extends LitElement {
       background: white;
       text-align: right;
     }
+
+    .cell {
+      padding: 0;
+      margin: 0;
+      width: 100%;
+      height: 100%;
+      box-sizing: border-box;
+      border: none;
+      border-bottom: 1px solid #ccc;
+      border-right: 1px solid #ccc;
+    }
+
+    .cell:focus {
+      outline-offset: -1px;
+      outline: 1px solid #0078d4;
+    }
   `;
 
   @state() private data = new Map<string, string>(
@@ -63,7 +79,20 @@ export class Cells extends LitElement {
           ROWS,
           (row) => html`
             <div class="row-header">${row}</div>
-            ${map(COLUMNS, (col) => html`<div class="cell">${col}${row}</div>`)}
+            ${map(
+              COLUMNS,
+              (col) =>
+                html` <input
+                  class="cell"
+                  type="text"
+                  .value=${this.data.get(`${col}${row}`) || ""}
+                  @input=${(e: Event) =>
+                    this.data.set(
+                      `${col}${row}`,
+                      (e.target as HTMLInputElement).value,
+                    )}
+                />`,
+            )}
           `,
         )}
       </div>
