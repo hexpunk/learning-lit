d05b6358ec67d7027203c2bd8eb5ec40494d74b9
hexpunk <10983817+hexpunk@users.noreply.github.com>
2026-03-26 19:09:51 -0500

    Disable Storybook telemetry

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,5 +4,8 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   framework: "@storybook/web-components-vite",
   addons: ["@github-ui/storybook-addon-performance-panel/universal"],
+  core: {
+    disableTelemetry: true,
+  },
 };
 export default config;
